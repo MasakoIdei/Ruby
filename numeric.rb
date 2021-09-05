7f1652bbb96794af4827a2dca8d0e4c03bdcb4b0
MasakoIdei <tarte.groseilles@gmail.com>
2021-09-05 09:58:50 +0000

    Rubyを学ぼう5章

---
+++ numeric.rb
@@ -0,0 +1,19 @@
+#四則演算の確認
+num = 100
+
+puts "基本になる数字は、100"
+
+puts "3を足す"
+puts num + 3
+
+puts "3を引く"
+puts num - 3
+
+puts "3をかける"
+puts num * 3
+
+puts "3で割る"
+puts num / 3
+
+puts "3で割った時のあまりを出す"
+puts num % 3
