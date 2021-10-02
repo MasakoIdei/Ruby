5cfdb4a65f549e43f98ae4a716174bcf9dc97600
MasakoIdei <tarte.groseilles@gmail.com>
2021-10-02 15:15:56 +0000

    真偽の復習

--- Ruby_book/country.rb
+++ Ruby_book/country.rb
@@ -2,6 +2,21 @@
 # どこの国のあいさつを知りたい？？　→　データの入力　→　結果を返す
 # 日本語、英語、中国語、韓国語、フランス語、ドイツ語、イタリア語
 
-puts "[アメリカ、中国、韓国、フランス、ドイツ、イタリア]"
-puts "どこの国のこんにちはを知りたいですか？？"
-puts "知りたい国の名前を打ち込んでください！"
+
+country = "America"
+
+  if country == "America"
+    puts "Hello!【ハロー】"
+  elsif country == "China"
+    puts "你好！【ニイハオ】"
+  elsif country == "Korea"
+    puts "안녕하세요！【アニョハセヨ】"
+  elsif country == "France"
+    puts "Bonjour！【ボンジュール】"
+  elsif country == "Germany"
+    puts "Guten tag！【グーテンターク】"
+  elsif country == "Itary"
+    puts "Buon giorno!【ボンジョルノ】"
+  else
+    puts "#{country}は登録されていません"
+  end
